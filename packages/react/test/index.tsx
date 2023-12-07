c8e33490f7fd9a7943beefb3336b67494edd258a
Ken Powers <ken@kenpowers.net>
2023-12-06 23:06:53 -0500

    Cleanup

--- packages/react/test/index.tsx
+++ packages/react/test/index.tsx
@@ -1,8 +1,8 @@
-import Icon1 from './icon1.svg?sprite-react';
-import Icon2 from './icon2.svg?sprite-react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import invariant from 'tiny-invariant';
+import Icon1 from './icon1.svg?sprite-react';
+import Icon2 from './icon2.svg?sprite-react';
 
 const el = document.getElementById('app');
 invariant(el);
